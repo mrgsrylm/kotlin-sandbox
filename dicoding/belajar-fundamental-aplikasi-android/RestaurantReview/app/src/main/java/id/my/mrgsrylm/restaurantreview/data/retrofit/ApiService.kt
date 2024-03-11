0c7059f87bb7af50db9758d141ff450a35c5baa8
Agus S. Mubarok <gusrylmubarok@gmail.com>
2024-03-11 18:15:14 +0700

    feat(dicoding): learn background thread and networking

---
+++ dicoding/belajar-fundamental-aplikasi-android/RestaurantReview/app/src/main/java/id/my/mrgsrylm/restaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,22 @@
+package id.my.mrgsrylm.restaurantreview.data.retrofit
+
+import id.my.mrgsrylm.restaurantreview.data.response.PostReviewResponse
+import id.my.mrgsrylm.restaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
+}
